229e7a7f1c19605f4c10a6d3a5077f1c49aca978
Jonathan Boyett <jonathan@failingservers.com>
2015-03-02 11:55:18 -0500

    manage file contents

--- src/file.c
+++ src/file.c
@@ -12,13 +12,33 @@ static int state = CM_PRESENT;
 static char *owner = NULL;
 static char *group = NULL;
 static char *mode = NULL;
+static char *source = NULL;
 static int noop = 0;
 static int verbose = 0;
 
-void run(void) {
-	int i;
+static cm_file *setup(void) {
 	cm_file *f = malloc(sizeof(cm_file));
 	cm_file_init(f);
+	return f;
+}
+
+static void teardown(cm_file *f) {
+	cm_file_deinit(f);
+	free(f);
+}
+
+static void get(char *file, cm_file *f) {
+	int i;
+	i = cm_file_get(file, f);
+	if(i != 0) {
+		teardown(f);
+		die("%s %s: Unable to get file info: %s\n", type, file, cm_strerror(i));
+	}
+}
+
+void run(void) {
+	int i;
+	cm_file *f = setup();
 	f->state = state;
 	if(owner) {
 		strcpy(f->owner, owner);
@@ -29,9 +49,11 @@ void run(void) {
 	if(mode) {
 		strcpy(f->mode, mode);
 	}
+	if(source) {
+		strcpy(f->source, source);
+	}
 	i = cm_file_set(path, f);
-	cm_file_deinit(f);
-	free(f);
+	teardown(f);
 	if(i != 0) {
 		die("%s %s: Unable to manage file: %s\n", type, path, cm_strerror(i));
 	}
@@ -40,16 +62,11 @@ void run(void) {
 
 void diff(void) {
 	int i;
-	cm_file *f = malloc(sizeof(cm_file));
-	cm_file_init(f);
-	i = cm_file_get(path, f);
-	if(i != 0) {
-		free(f);
-		die("%s %s: Unable to get file info: %s\n", type, path, cm_strerror(i));
-	}
+	cm_file *f = setup();
+	get(path, f);
 	i = diff_state(state, f->state, type, path);
 	if(i != 0) {
-		free(f);
+		teardown(f);
 		return;
 	}
 	if(state == CM_PRESENT) {
@@ -62,9 +79,15 @@ void diff(void) {
 		if(mode) {
 			diff_string(mode, f->mode, type, "mode", path);
 		}
+		if(source) {
+			cm_file *s = setup();
+			get(source, s);
+			diff_string(f->hash, s->hash, type, "content hash", path);
+			teardown(s);
+		}
+			
 	}
-	cm_file_deinit(f);
-	free(f);
+	teardown(f);
 	return;
 }
 
@@ -78,6 +101,7 @@ void usage(void) {
 			"\t-m, --mode MODE      file mode (4 digit octal)\n"
 			"\t-n, --noop           print but do not commit changes\n"
 			"\t-o, --owner OWNER    file owner\n"
+			"\t-s, --source PATH    source file path\n"
 			"\t-v, --verbose        enable verbose output\n");
 	exit(EXIT_FAILURE);
 }
@@ -95,11 +119,12 @@ int main(int argc, char *argv[]) {
 		{"mode", required_argument, NULL, 'm'},
 		{"noop", no_argument, NULL, 'n'},
 		{"owner", required_argument, NULL, 'o'},
+		{"source", required_argument, NULL, 's'},
 		{"verbose", no_argument, NULL, 'v'},
 		{NULL, 0, NULL, '\0'}
 	};
 
-	while((i = getopt_long(argc, argv, "ag:hm:no:t:v", lopts, NULL)) != -1) {
+	while((i = getopt_long(argc, argv, "ag:hm:no:s:v", lopts, NULL)) != -1) {
 		switch(i) {
 			case 'a':
 				state = CM_ABSENT;
@@ -115,6 +140,9 @@ int main(int argc, char *argv[]) {
 				break;
 			case 'o':
 				owner = optarg;
+				break;
+			case 's':
+				source = optarg;
 				break;
 			case 'v':
 				verbose = 1;
